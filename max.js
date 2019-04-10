c0318219e41d197076d55ac2058e70648e50c21d
Zack Brody <brodyzt@gmail.com>
2019-04-10 07:12:33 -0400

    commit for in class presentations. decently working graphs but still need a lot of work

--- max.js
+++ max.js
@@ -13,129 +13,479 @@ var margin = {
     height = svg.attr("height") - margin.top - margin.bottom;
 
 
-const getNetwork = async () => {
-
-    const data = await d3.json("testNodes.json");
-
-    const links = data.links.map(d => Object.create(d));
-    const nodes = data.nodes.map(d => Object.create(d));
-
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
-
-    const link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-        .selectAll("line")
-        .data(links)
-        .join("line")
-        .attr("stroke-width", d => Math.sqrt(d.value));
 
 
-    const node = svg.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 2)
-        .selectAll("circle")
-        .data(nodes)
-        .join("circle")
-        .attr("r", 5)
-        // .call(d3.drag()
-        //     // .on("start", function (d) {
-        //     //     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-        //     //     d.fx = d.x;
-        //     //     d.fy = d.y;
-        //     // })
-        //     // .on("drag", function (d) {
-        //     //     d.fx = d3.event.x;
-        //     //     d.fy = d3.event.y;
-        //     // })
-        //     .on("end", d => {
-        //         if (!d3.event.active) simulation.alphaTarget(0);
-        //         d.fx = null;
-        //         d.fy = null;
-        //     })
-        //     (simulation));
-        .call(drag(simulation));
-    
-    function drag(simulation) {
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-          }
-          
-          function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-          }
-          
-          function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
-          }
-          
-          return d3.drag()
-              .on("start", dragstarted)
-              .on("drag", dragged)
-              .on("end", dragended);
-    }
+const startup = async () => {
 
-    node.append("title")
-        .text(d => d.id);
+    var options = {
+        keys: ['name'],
+        id: 'id'
+    };
 
-    simulation.on("tick", () => {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+    const fuse_data = await d3.json("fuse.json");
+    const id_to_data = await d3.json("id_to_data.json");
 
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-    });
-
-    invalidation.then(() => simulation.stop());
-
-    return svg.node();
-
-};
-
-getNetwork();
-
-
-
-var options = {
-    keys: ['name'],
-    id: 'name'
-};
-
-
-const getInput = async () => {
-
-    const data = await d3.json("scraped_data_reformated.json");
-
-    var fuse = new Fuse(data, options)
+    var fuse = new Fuse(fuse_data, options)
     console.log(fuse.search("old"))
 
     var body = d3.select('body')
 
-    body.append('input')
-        .attr('type', 'text')
-        .attr('name', 'textInput')
+    d3.select("input#textInput")
         .on("input", function () {
             titleSearch(this.value);
         });
 
     function titleSearch(title) {
-        console.log(fuse.search(title));
+        const results = fuse.search(title);
+        console.log(id_to_data[results[0]])
+        update_force_graph(results[0]);
     }
+
+
+    //////////////////////////////////////////////////////////////////
+
+
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var nodes = [],
+        links = [];
+
+
+    var simulation = d3.forceSimulation(nodes)
+        .force("charge", d3.forceManyBody().strength(-1000))
+        .force("link", d3.forceLink(links).distance(200).id(d => d.id))
+        .force("x", d3.forceX())
+        .force("y", d3.forceY())
+        .alphaTarget(1)
+        .on("tick", ticked);
+
+    var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
+        link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1.5).selectAll(".link"),
+        node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
+
+    // d3.timeout(function () {
+    //     links.push({
+    //         source: a,
+    //         target: b
+    //     }); // Add a-b.
+    //     links.push({
+    //         source: b,
+    //         target: c
+    //     }); // Add b-c.
+    //     links.push({
+    //         source: c,
+    //         target: a
+    //     }); // Add c-a.
+    //     restart();
+    // }, 1000);
+
+    // d3.interval(function () {
+    //     nodes.pop(); // Remove c.
+    //     links.pop(); // Remove c-a.
+    //     links.pop(); // Remove b-c.
+    //     restart();
+    // }, 2000, d3.now());
+
+    // d3.interval(function () {
+    //     nodes.push(c); // Re-add c.
+    //     links.push({
+    //         source: b,
+    //         target: c
+    //     }); // Re-add b-c.
+    //     links.push({
+    //         source: c,
+    //         target: a
+    //     }); // Re-add c-a.
+    //     restart();
+    // }, 2000, d3.now() + 1000);
+
+    function update_force_graph(game_id) {
+
+        nodes = []
+        links = []
+
+        const num_layers = 3;
+        const max_per_layer = 7;
+
+        let added = new Set();
+
+        add_similar(game_id, num_layers);
+
+        function add_similar(game_id, level) {
+            // console.log(game_id)
+            game_data = id_to_data[game_id];
+            console.log(game_data)
+            if (game_data && !(game_id in added)) {
+                added.add(game_id)
+                nodes.push(Object.create({
+                    id: game_id,
+                    name: id_to_data[game_id]["name"]
+                }))
+
+                if (level - 1 > 0) {
+                    similar_games = game_data["similar_games"].filter(d => d in id_to_data).filter(d => !(d in added));
+                    console.log(similar_games)
+                    similar_games.forEach(d => {
+                        links.push(Object.create({
+                            source: game_id,
+                            target: d
+                        }));
+                    })
+                    similar_games.forEach(d => add_similar(d, level - 1));
+                }
+            }
+        }
+
+        console.log(links)
+        console.log(nodes)
+
+        // nodes = similar_game_ids.filter(d => d in id_to_data).map(d => {
+        //     console.log("This: " + id_to_data[d]["name"]);
+        //     return Object.create({
+
+        //     })
+        // });
+        // nodes.push(Object.create({
+        //     id: game_id,
+        //     name: id_to_data[game_id]["name"]
+        // }));
+        // links = similar_game_ids.filter(d => d in id_to_data).map(d => Object.create({
+        //     source: game_id,
+        //     target: d
+        // }))
+
+        // Apply the general update pattern to the nodes.
+        node = node.data(nodes, function (d) {
+            return d.id;
+        });
+        node.exit().remove();
+        node = node.enter().append("circle").attr("fill", function (d) {
+                return color(d.id);
+            }).attr("r", 8)
+            .call(drag(simulation))
+            .merge(node)
+            .on("mouseover", function (d, i) {
+                console.log(d.name)
+            });
+
+        // Apply the general update pattern to the links.
+        link = link.data(links, function (d) {
+            return d.source.id + "-" + d.target.id;
+        });
+        link.exit().remove();
+        link = link.enter().append("line").merge(link);
+
+        // Update and restart the simulation.
+        simulation.nodes(nodes);
+        simulation.force("link").links(links);
+        simulation.alpha(1).restart();
+    }
+
+    function drag(simulation) {
+        function dragstarted(d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        function dragged(d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        }
+
+        function dragended(d) {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+
+        return d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+    }
+
+    update_force_graph(979);
+
+    function ticked() {
+        node.attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            })
+
+        link.attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let default_radius = 15;
+    // let initial_game_id = "979";
+    // let game_data = id_to_data[initial_game_id];
+    // let similar_game_ids = game_data["similar_games"];
+
+    // var links = []
+    // var nodes = []
+
+    // var simulation = d3.forceSimulation(nodes)
+    //     .force("link", d3.forceLink(links).id(d => d.id).distance(100))
+    //     .force("charge", d3.forceManyBody())
+    //     .force("center", d3.forceCenter(width / 2, height / 2))
+
+
+
+    // var linkg = svg.append("g");
+    // // var link = linkg.attr("stroke", "#999")
+    // //     .attr("stroke-opacity", 0.6)
+    // //     .selectAll("line")
+    // //     .data(links)
+    // //     .join("line")
+    // //     .attr("stroke-width", d => Math.sqrt(d.value));
+
+
+    // var nodeg = svg.append("g");
+    // // var node = nodeg.attr("stroke", "#fff")
+    // //     .attr("stroke-width", 2)
+    // //     .selectAll("circle")
+    // //     .data(nodes)
+    // //     .join("circle")
+    // //     .attr("r", default_radius)
+    // //     .on("mouseover", function (d, i) {
+    // //         const circle = d3.select(this);
+    // //         console.log(circle.attr("r"));
+
+    // //         let textElement = d3.select(this).append("text")
+    // //             .attr("x", circle.attr("cx") + 20)
+    // //             .attr("y", circle.attr("cy") + 20)
+    // //             .attr("id", "dataLabel")
+    // //             .attr("font-size", "25")
+    // //             .style("background-color", "red")
+    // //             .text(d => d.name);
+
+    // //         // enlarge circle
+    // //         circle.transition()
+    // //             .duration(50)
+    // //             .attr("r", default_radius * 2)
+
+
+    // //     })
+    // //     .on("mouseout", function () {
+    // //         document.getElementById("dataLabel").remove();
+    // //         const circle = d3.select(this);
+
+    // //         // shrink circle to original size
+    // //         circle.transition()
+    // //             .duration(50)
+    // //             .attr("r", default_radius)
+
+    // //     })
+    // // .call(d3.drag()
+    // //     // .on("start", function (d) {
+    // //     //     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    // //     //     d.fx = d.x;
+    // //     //     d.fy = d.y;
+    // //     // })
+    // //     // .on("drag", function (d) {
+    // //     //     d.fx = d3.event.x;
+    // //     //     d.fy = d3.event.y;
+    // //     // })
+    // //     .on("end", d => {
+    // //         if (!d3.event.active) simulation.alphaTarget(0);
+    // //         d.fx = null;
+    // //         d.fy = null;
+    // //     })
+    // //     (simulation));
+    // // .call(drag(simulation));
+
+
+    // // node.append("title")
+    // //     .text(d => d.name);
+
+    // // simulation.on("tick", () => {
+    // //     link
+    // //         .attr("x1", d => d.source.x)
+    // //         .attr("y1", d => d.source.y)
+    // //         .attr("x2", d => d.target.x)
+    // //         .attr("y2", d => d.target.y);
+
+    // //     node
+    // //         .attr("cx", d => d.x)
+    // //         .attr("cy", d => d.y);
+    // // });
+
+
+    // function update_force_graph(game_id) {
+    //     console.log("updating to: " + game_id)
+
+    //     let game_data = id_to_data[game_id];
+    //     let similar_game_ids = game_data["similar_games"];
+
+    //     links = similar_game_ids.filter(d => d in id_to_data).map(d => Object.create({
+    //         source: game_id,
+    //         target: d
+    //     }))
+    //     nodes = similar_game_ids.filter(d => d in id_to_data).map(d => {
+    //         console.log("This: " + id_to_data[d]["name"]);
+    //         return Object.create({
+    //             id: d,
+    //             name: id_to_data[d]["name"]
+    //         })
+    //     });
+    //     nodes.push(Object.create({
+    //         id: game_id,
+    //         name: id_to_data[game_id]["name"]
+    //     }));
+
+    //     // console.log(node.selectAll("circle"))
+
+    //     // console.log(node)
+
+    //     let node = nodeg.selectAll("circle")
+    //         .data(nodes);
+
+    //     node.enter()
+    //         .append("circle")
+    //         .attr("cx", 0)
+    //         .attr("cy", 0)
+    //         .attr("r", default_radius)
+    //         // .attr("test", d => console.log(d))
+    //         // .select("title")
+    //         // .text(d => d.name)
+    //         // .on("mouseover", function (d, i) {
+    //         //     const circle = d3.select(this);
+    //         //     console.log(circle.attr("r"));
+
+    //         //     let textElement = d3.select(this).append("text")
+    //         //         .attr("x", circle.attr("cx") + 20)
+    //         //         .attr("y", circle.attr("cy") + 20)
+    //         //         .attr("id", "dataLabel")
+    //         //         .attr("font-size", "25")
+    //         //         .style("background-color", "red")
+    //         //         .text(d => d.name);
+
+    //         //     // enlarge circle
+    //         //     circle.transition()
+    //         //         .duration(50)
+    //         //         .attr("r", default_radius * 2)
+
+
+    //         // })
+    //         // .on("mouseout", function () {
+    //         //     document.getElementById("dataLabel").remove();
+    //         //     const circle = d3.select(this);
+
+    //         //     // shrink circle to original size
+    //         //     circle.transition()
+    //         //         .duration(50)
+    //         //         .attr("r", default_radius)
+
+    //         // })
+    //         .merge(node)
+    //         .attr("transform", d => "translate(" + d.x + "," + console.log("here") + d.y + ")")
+    //         // .call(d3.drag()
+    //         //     // .on("start", function (d) {
+    //         //     //     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    //         //     //     d.fx = d.x;
+    //         //     //     d.fy = d.y;
+    //         //     // })
+    //         //     // .on("drag", function (d) {
+    //         //     //     d.fx = d3.event.x;
+    //         //     //     d.fy = d3.event.y;
+    //         //     // })
+    //         //     .on("end", d => {
+    //         //         if (!d3.event.active) simulation.alphaTarget(0);
+    //         //         d.fx = null;
+    //         //         d.fy = null;
+    //         //     })
+    //         //     (simulation));
+    //         .call(drag(simulation))
+
+    //     node.exit()
+    //         .remove();
+
+    //     let link = linkg.selectAll("line").data(links);
+
+    //     link.exit()
+    //         .remove();
+
+    //     link.data(links)
+    //         .attr("x1", d => console.log(d.source.x))
+    //         .enter()
+    //         .append("line")
+    //         .merge(link)
+    //         .attr("x2", d => d.target.x)
+    //         .attr("y1", d => d.source.y)
+    //         .attr("y2", d => d.target.y)
+
+    //     console.log(nodes)
+    //     console.log(link)
+    //     simulation.nodes(nodes);
+    //     simulation.force("link").links(links);
+    //     simulation.restart();
+    // }
+
+    // // Initial Value
+
+
+    // function drag(simulation) {
+    //     function dragstarted(d) {
+    //         if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    //         d.fx = d.x;
+    //         d.fy = d.y;
+    //     }
+
+    //     function dragged(d) {
+    //         d.fx = d3.event.x;
+    //         d.fy = d3.event.y;
+    //     }
+
+    //     function dragended(d) {
+    //         if (!d3.event.active) simulation.alphaTarget(0);
+    //         d.fx = null;
+    //         d.fy = null;
+    //     }
+
+    //     return d3.drag()
+    //         .on("start", dragstarted)
+    //         .on("drag", dragged)
+    //         .on("end", dragended);
+    // }
+
+    // update_force_graph(initial_game_id);
+
+    // invalidation.then(() => simulation.stop());
+
+    // return svg.node();
+
 };
 
-getInput();
+startup();
 
 
 
