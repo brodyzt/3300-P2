caff67c262fdd6ee9bc01686f26ff6f889e17634
Zack Brody <brodyzt@gmail.com>
2019-04-11 20:05:39 -0400

    created popup info box. need to work on descriptions

--- script.js
+++ script.js
@@ -1,57 +1,37 @@
-
-
-// let text_space = info_div.append("span")
-
-// console.log(info_div)
-
-let help_boxes = d3.selectAll(".help-box")
+let info_buttons = d3.selectAll(".info-button")
     .on("mouseover", mouseOver)
     .on("mouseleave", mouseOut)
+    .on("click", clicked)
 
 
-const barGraphDescription = "This is a bar"
+const barGraphDescription = "On this graph you are able to select that attributes that are being compared. Using the two dropdowns on the right, the categories on both the axes can be changed and the graph will automatically reload. If hovered over, a category will be highlighted both on the graph itself and within the legend. This tool can be used to analyze the trend of a specific category."
 
 const networkDescription = "This is a network"
+
+
+function clicked() {
+    let self = d3.select(this);
+
+    let popup_html = '<html><body><span style="color:white;font-size:30px;">' + (self.attr("id") == "bar-graph-help" ? barGraphDescription : networkDescription) + '</span></body></html>';
+
+    lity("bar_graph_description.html")
+
+}
 
 function mouseOver() {
     let self = d3.select(this);
 
     self.transition()
-    .duration(300)
-    .style("background", self.attr("color"))
-    .select("span")
-    .style("color","black")
-
-    console.log(self.attr("id"))
-    console.log(self.attr("x"))
-
-    if(self.attr("id") == "bar-graph-help") {
-        console.log(barGraphDescription)
-    } else {
-        console.log(networkDescription)
-    }
-
-    // let info_div = d3.select("body")
-    // .append("div")
-    // .attr("background", "red")
-
-    lity('<html><body><span style="color:red">' + (self.attr("id") == "bar-graph-help" ? barGraphDescription : networkDescription) + '</span></body></html>')
-
-    
-    // .attr("")
-
-    // console.log(this)
-    // console.log(d)
-
+        .duration(300)
+        .style("background", self.attr("color"))
+        .style("color", "black")
 }
 
 function mouseOut(d) {
     let self = d3.select(this);
-    
+
     self.transition()
-    .duration(300)
-    .style("background", "black")
-    .select("span")
-    .style("color",self.attr("color"))
-    console.log("leaving")
+        .duration(300)
+        .style("background", "black")
+        .style("color", self.attr("color"))
 }
